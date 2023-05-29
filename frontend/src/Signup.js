ef12a4a60e9e3b5bbc7b05b282506e51415ed38a
talhaarshd <123795938+talhaarshd@users.noreply.github.com>
2023-05-29 21:59:26 +0530

    added category field

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -7,7 +7,8 @@ function Signup() {
         {
             email: '',
             password: '',
-            name: ''
+            name: '',
+            category: ['beneficiary']
         }
     )
     const navigate = useNavigate();
@@ -18,7 +19,17 @@ function Signup() {
         if (errors.name === "" && errors.email === "" && errors.password === "") {
             axios.post('http://localhost:8081/signup', values)
                 .then(res => {
-                    navigate('/');
+                    console.log(res.data);
+                    if (res.data === 'Duplicate') {
+                        //error notification
+                        alert("Email already registerd");
+                    }
+                    else if (res.data === "Error") {
+                        alert("Internal Server Error");
+                    }
+                    else {
+                        navigate('/');
+                    }
                 })
                 .catch(err => console.log(err));
         }
@@ -33,19 +44,34 @@ function Signup() {
                 <form action="" onSubmit={handleSubmit}>
                     <div className='mb-3'>
                         <label style={{ color: 'white' }} htmlFor="name">Name</label>
-                        <input style={{ backgroundColor: '#13131A' }} type="text" placeholder='Enter Name' name='name' onChange={handleInput} className='form-control rounded-0'></input>
+                        <input style={{ backgroundColor: '#13131A', color: 'white' }} type="text" placeholder='Enter Name' name='name' onChange={handleInput} className='form-control rounded-0'></input>
                         {errors.name && <span className='text-danger'>{errors.name}</span>}
                     </div>
                     <div className='mb-3'>
                         <label style={{ color: 'white' }} htmlFor="email">Email</label>
-                        <input style={{ backgroundColor: '#13131A' }} type="email" placeholder='Enter Email' name='email' onChange={handleInput} className='form-control rounded-0'></input>
+                        <input style={{ backgroundColor: '#13131A', color: 'white' }} type="email" placeholder='Enter Email' name='email' onChange={handleInput} className='form-control rounded-0'></input>
                         {errors.email && <span className='text-danger'>{errors.email}</span>}
                     </div>
                     <div className='mb-3'>
                         <label style={{ color: 'white' }} htmlFor="password">Password</label>
-                        <input style={{ backgroundColor: '#13131A' }} type="password" placeholder='Enter Password' name='password' onChange={handleInput} className='form-control rounded-0'></input>
+                        <input style={{ backgroundColor: '#13131A', color: 'white' }} type="password" placeholder='Enter Password' name='password' onChange={handleInput} className='form-control rounded-0'></input>
                         {errors.password && <span className='text-danger'>{errors.password}</span>}
                     </div>
+                    <div className='mb-3'>
+                        <label style={{ color: 'white' }} htmlFor="category">Category of user</label>
+                        <select
+                            style={{ backgroundColor: '#13131A', color: 'white' }}
+                            name='category'
+                            onChange={handleInput}
+                            className='form-control rounded-0'
+                        >
+                            <option value='beneficiary'>Beneficiary</option>
+                            <option value='donor'>Donor</option>
+                            <option value='store'>Store</option>
+                            <option value='charity'>Charity Organisation</option>
+                        </select>
+                    </div>
+
                     <button type='submit' className='btn btn-success w-100 rounded-0 rounded-pill' style={{ backgroundColor: '#4ACD8D' }}>Sign Up</button>
                     <p style={{ color: 'white' }}>You agree to out terms and policies.</p>
                     <Link to="/" className='btn btn-default border w-100 bg-light rounded-0 rounded-pill'>Login</Link>
